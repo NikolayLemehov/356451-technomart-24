44e637b709e85be8ea304d719868accf1f627b97
Nikolay Liemiekhov <nicklemster@gmail.com>
2019-02-20 08:04:26 +0200

    Оживляет карту.

--- js/script.js
+++ js/script.js
@@ -1,14 +1,26 @@
 var openPopupButtonFlex = document.querySelector('.about-us__btn');
 var popupWriteUs = document.querySelector('.modal.write-us');
-var closePopupButton = popupWriteUs.querySelector('.btn-modal-close');
+var closePopupButtonWriteUs = popupWriteUs.querySelector('.btn-modal-close');
+
+var openMap = document.querySelector('.contacts__img');
+var popupMap = document.querySelector('.modal-map');
+var closePopupButtonMap = popupMap.querySelector('.btn-modal-close');
+// var closePopupButtons = document.querySelectorAll('.btn-modal-close');
 
 openPopupButtonFlex.addEventListener('click', function (evt) {
   evt.preventDefault();
-  popupWriteUs.classList.add('modal__show_flex');
+  popupWriteUs.classList.add('modal__show_flex')
 });
 
-closePopupButton.addEventListener('click', function () {
-  popupWriteUs.classList.remove('modal__show_flex');
-  popupWriteUs.classList.remove('modal__show_block');
+closePopupButtonWriteUs.addEventListener('click', function () {
+  popupWriteUs.classList.remove('modal__show_flex')
+});
+
+openMap.addEventListener('click', function () {
+  popupMap.classList.add('modal__show_block')
+});
+
+closePopupButtonMap.addEventListener('click', function () {
+  popupMap.classList.remove('modal__show_block');
 });
 
