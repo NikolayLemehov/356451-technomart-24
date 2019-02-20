db5deee197408c58cbeeea4c8174c9b37bd43dfb
Nikolay Liemiekhov <nicklemster@gmail.com>
2019-02-20 05:29:09 +0200

    Разделяет профиль и выход в header согласно пункту 3.4. ТЗ.

--- js/script.js
+++ js/script.js
@@ -1,13 +1,20 @@
 var popup = document.querySelector('.modal.write-us');
-var openPopupButton = document.querySelector('.btn-default-contacts');
+var openPopupButtonFlex = document.querySelector('.btn-default-contacts');
+var openPopupButtonBlock = document.querySelector('.btn-default-contacts');
 var closePopupButton = popup.querySelector('.btn-modal-close');
 
-openPopupButton.addEventListener('click', function (evt) {
+openPopupButtonFlex.addEventListener('click', function (evt) {
   evt.preventDefault();
   popup.classList.add('modal__show_flex');
 });
 
+openPopupButtonBlock.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  popup.classList.add('modal__show_block');
+});
+
 closePopupButton.addEventListener('click', function () {
   popup.classList.remove('modal__show_flex');
+  popup.classList.remove('modal__show_block');
 });
 
