8cca5c3ffd842421641e6d303e42c898c6c83603
Nikolay Liemiekhov <nicklemster@gmail.com>
2019-02-22 03:51:02 +0200

    Оживляет попап корзины для goods-card. Объеденяет в функции каждый попап.

--- js/script.js
+++ js/script.js
@@ -1,25 +1,51 @@
-var openPopupButtonFlex = document.querySelector('.about-us__btn');
-var popupWriteUs = document.querySelector('.modal.write-us');
-var closePopupButtonWriteUs = popupWriteUs.querySelector('.btn-modal-close');
+function writeUs() {
+  var openPopupButton = document.querySelector('.about-us__btn');
+  var popup = document.querySelector('.modal.write-us');
+  var closePopupButton = popup.querySelector('.btn-modal-close');
 
-var openMap = document.querySelector('.contacts__img');
-var popupMap = document.querySelector('.modal-map');
-var closePopupButtonMap = popupMap.querySelector('.btn-modal-close');
+  openPopupButton.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    popup.classList.add('modal__show_flex')
+  });
 
-openPopupButtonFlex.addEventListener('click', function (evt) {
-  evt.preventDefault();
-  popupWriteUs.classList.add('modal__show_flex')
-});
+  closePopupButton.addEventListener('click', function () {
+    popup.classList.remove('modal__show_flex')
+  });
+}
 
-closePopupButtonWriteUs.addEventListener('click', function () {
-  popupWriteUs.classList.remove('modal__show_flex')
-});
+function map() {
+  var openMap = document.querySelector('.contacts__img');
+  var popup = document.querySelector('.modal-map');
+  var closePopupButton = popup.querySelector('.btn-modal-close');
 
-openMap.addEventListener('click', function () {
-  popupMap.classList.add('modal__show_block')
-});
+  openMap.addEventListener('click', function () {
+    popup.classList.add('modal__show_block')
+  });
 
-closePopupButtonMap.addEventListener('click', function () {
-  popupMap.classList.remove('modal__show_block');
-});
+  closePopupButton.addEventListener('click', function () {
+    popup.classList.remove('modal__show_block');
+  });
+}
+
+function goodsCardBasket() {
+  var openPopupButtons = document.querySelectorAll('.goods-card__link_buy');
+  var popup = document.querySelector('.modal-add-basket');
+  var closePopupButton = popup.querySelector('.btn-modal-close');
+
+  for (var i = 0; i < openPopupButtons.length; i++) {
+    var openPopupButton = openPopupButtons[i];
+    openPopupButton.addEventListener('click', function (evt) {
+      evt.preventDefault();
+      popup.classList.add('modal__show_flex')
+    });
+  }
+
+  closePopupButton.addEventListener('click', function () {
+    popup.classList.remove('modal__show_flex')
+  });
+}
+
+writeUs();
+map();
+goodsCardBasket();
 
