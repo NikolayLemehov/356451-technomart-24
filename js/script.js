366836404ba9d537a80ee564a4b155306d6426e5
Nikolay Liemiekhov <nicklemster@gmail.com>
2019-02-25 00:40:38 +0200

    Добавляет в форме попапа write-us localStrorage, управление фокусом, закрытие попапа по клавише Esc.

--- js/script.js
+++ js/script.js
@@ -4,15 +4,60 @@ function writeUs() {
   var openPopupButton = document.querySelector('.about-us__btn');
   var popup = document.querySelector('.modal.write-us');
   var closePopupButton = popup.querySelector('.btn-modal-close');
+  var form = popup.querySelector('form');
+  var nameField = popup.querySelector('[name=name]');
+  var emailField = popup.querySelector('[name=email]');
+  var textField = popup.querySelector('[name=text]');
+  var isStorageSupport = true;
+  var nameStorage = '';
+  var emailStorage = '';
 
+  try {
+    nameStorage = localStorage.getItem('nameField');
+    emailStorage = localStorage.getItem('emailField');
+  } catch (err) {
+    isStorageSupport = false;
+  }
   openPopupButton.addEventListener('click', function (evt) {
     evt.preventDefault();
-    popup.classList.add('modal__show_flex')
+    popup.classList.add('modal__show_flex');
+    if (nameStorage) {
+      nameField.value = nameStorage;
+    } else {
+      nameField.focus();
+    }
+    if (emailStorage) {
+      emailField.value = emailStorage;
+    }
+    if (nameStorage && !emailStorage) {
+      emailField.focus();
+    }
+    if (nameStorage && emailStorage) {
+      textField.focus();
+    }
   });
-
   closePopupButton.addEventListener('click', function (evt) {
     evt.preventDefault();
-    popup.classList.remove('modal__show_flex')
+    popup.classList.remove('modal__show_flex');
+  });
+  form.addEventListener('submit', function (evt) {
+    if (!nameField.value || !emailField.value || !textField.value) {
+      evt.preventDefault();
+      console.log('Нужно ввести пароль');
+    } else {
+      if (isStorageSupport) {
+        localStorage.setItem('nameField', nameField.value);
+        localStorage.setItem('emailField', emailField.value);
+      }
+    }
+  });
+  window.addEventListener('keydown', function (evt) {
+    if (evt.keyCode === 27) {
+      evt.preventDefault();
+      if (popup.classList.contains('modal__show_flex')) {
+        popup.classList.remove('modal__show_flex');
+      }
+    }
   });
 }
 
@@ -23,9 +68,8 @@ function map() {
 
   openMap.addEventListener('click', function (evt) {
     evt.preventDefault();
-    popup.classList.add('modal__show_block')
+    popup.classList.add('modal__show_block');
   });
-
   closePopupButton.addEventListener('click', function (evt) {
     evt.preventDefault();
     popup.classList.remove('modal__show_block');
@@ -39,15 +83,15 @@ function goodsCardBasket() {
 
   for (var i = 0; i < openPopupButtonList.length; i++) {
     var openPopupButton = openPopupButtonList[i];
+
     openPopupButton.addEventListener('click', function (evt) {
       evt.preventDefault();
-      popup.classList.add('modal__show_flex')
+      popup.classList.add('modal__show_flex');
     });
   }
-
   closePopupButton.addEventListener('click', function (evt) {
     evt.preventDefault();
-    popup.classList.remove('modal__show_flex')
+    popup.classList.remove('modal__show_flex');
   });
 }
 
@@ -81,7 +125,7 @@ function slider() {
       dot.classList.add('promo-slider__dot-btn_active');
       sliderActive.classList.remove('visible');
       slider.classList.add('visible');
-    })
+    });
   };
 
   nextButton.addEventListener('click', function () {
@@ -91,16 +135,14 @@ function slider() {
         sliderDotList[i].classList.remove('promo-slider__dot-btn_active');
         sliderList[0].classList.add('visible');
         sliderDotList[0].classList.add('promo-slider__dot-btn_active');
-        console.log('else' + i);
-        return
+        return;
       }
       if (sliderList[i].classList.contains('visible') && 0 <= i && i < sliderList.length - 1) {
         sliderList[i].classList.remove('visible');
         sliderDotList[i].classList.remove('promo-slider__dot-btn_active');
         sliderList[i + 1].classList.add('visible');
         sliderDotList[i + 1].classList.add('promo-slider__dot-btn_active');
-        console.log('if' + i);
-        return
+        return;
       }
     }
   });
@@ -114,18 +156,17 @@ function slider() {
         sliderDotList[i].classList.remove('promo-slider__dot-btn_active');
         sliderList[maxI].classList.add('visible');
         sliderDotList[maxI].classList.add('promo-slider__dot-btn_active');
-        return
+        return;
       }
       if (sliderList[i].classList.contains('visible') && 0 < i && i <= sliderList.length - 1) {
         sliderList[i].classList.remove('visible');
         sliderDotList[i].classList.remove('promo-slider__dot-btn_active');
         sliderList[i - 1].classList.add('visible');
         sliderDotList[i - 1].classList.add('promo-slider__dot-btn_active');
-        return
+        return;
       }
     }
   });
-
   for (var i = 0; i < sliderDotList.length; i++) {
     addDotClickHandler(sliderDotList[i], sliderList[i]);
   }
@@ -139,7 +180,7 @@ if (currentLocation.pathname.lastIndexOf('/356451-technomart-24/index.html') >= 
   slider();
 }
 
-if (currentLocation.pathname.lastIndexOf("/356451-technomart-24/catalog.html") >= 0) {
+if (currentLocation.pathname.lastIndexOf('/356451-technomart-24/catalog.html') >= 0) {
   goodsCardBasket();
 }
 
